a2b5017bf4c451c9ed6843b418ec34df69078e9c
Reut Vladislav <reut-2011@mail.ru>
2023-12-17 15:00:31 +0300

    feat: add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof (sampleActivity) !== 'string') {
+    return false;
+  }
+  if (sampleActivity <= 0 || sampleActivity > 15) {
+    return false;
+  }
+
+  let k = parseFloat('0.693') / HALF_LIFE_PERIOD;
+  if (sampleActivity.includes('.')) {
+    let time = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / k;
+    return Math.ceil(time);
+  } else {
+    let time = Math.log(MODERN_ACTIVITY / parseInt(sampleActivity)) / k;
+    return Math.ceil(time);
+  };
 }
 
 module.exports = {
