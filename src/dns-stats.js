ab8f718995e1f8024af4d91ccea5701b7a9a3479
Reut Vladislav <reut-2011@mail.ru>
2023-12-17 21:26:27 +0300

    feat: add function dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const resultObject = {};
+
+  for (let i = 0; i < domains.length; i++) {
+    const domainParts = domains[i].split('.');
+    let dnsPart = '';
+
+    // Обратный цикл, чтобы строить DNS-путь снизу вверх
+    for (let j = domainParts.length - 1; j >= 0; j--) {
+      dnsPart += `.${domainParts[j]}`;
+
+      // Проверка, существует ли уже путь в объекте result
+      if (resultObject[dnsPart]) {
+        resultObject[dnsPart]++;
+      } else {
+        resultObject[dnsPart] = 1;
+      }
+    }
+  }
+
+  return resultObject;
 }
 
 module.exports = {
