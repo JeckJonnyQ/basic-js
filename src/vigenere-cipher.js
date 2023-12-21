d2c0db4a95a36ccd9335ffa3964e7bb866c0d042
Reut Vladislav <reut-2011@mail.ru>
2023-12-21 11:28:21 +0300

    feat: add function vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,99 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse = true) {
+    this.reverse = reverse;
+    this.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+      'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+      'U', 'V', 'W', 'X', 'Y', 'Z'];
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let encryptedMessage = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const currentLetter = message[i];
+      if (this.isLetter(currentLetter)) {
+        const messageCharCode = this.getCharCode(currentLetter);
+        const keyCharCode = this.getCharCode(key[keyIndex]);
+        const encryptedCharCode = (messageCharCode + keyCharCode) % 26;
+        const encryptedLetter = this.getLetter(encryptedCharCode);
+        encryptedMessage += encryptedLetter;
+
+        keyIndex++;
+        if (keyIndex === key.length) {
+          keyIndex = 0;
+        }
+      } else {
+        encryptedMessage += currentLetter;
+      }
+    }
+
+    if (this.reverse) {
+      return encryptedMessage.split('').join('');
+    } else {
+      return this.reverseString(encryptedMessage).split('').join('');
+    }
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let decryptedMessage = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      const currentLetter = encryptedMessage[i];
+      if (this.isLetter(currentLetter)) {
+        const encryptedCharCode = this.getCharCode(currentLetter);
+        const keyCharCode = this.getCharCode(key[keyIndex]);
+        const decryptedCharCode = (encryptedCharCode - keyCharCode + 26) % 26;
+        const decryptedLetter = this.getLetter(decryptedCharCode);
+        decryptedMessage += decryptedLetter;
+
+        keyIndex++;
+        if (keyIndex === key.length) {
+          keyIndex = 0;
+        }
+      } else {
+        decryptedMessage += currentLetter;
+      }
+    }
+
+    if (this.reverse) {
+      return decryptedMessage.split('').join('');
+    } else {
+      return this.reverseString(decryptedMessage).split('').join('');
+    }
+  }
+
+  isLetter(letter) {
+    return this.alphabet.includes(letter);
+  }
+
+  getCharCode(letter) {
+    return letter.charCodeAt(0) - 65;
+  }
+
+  getLetter(charCode) {
+    return this.alphabet[charCode];
+  }
+
+  reverseString(string) {
+    return string.split('').reverse().join('');
   }
 }
 
